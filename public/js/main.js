b23b90f03234a05ded2800d3c28e5d03a94e7801
djedynak259 <dajed@outlook.com>
2016-11-18 08:18:06 -0800

    cmt1

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,24 @@
+(function() {
 
-angular.module('inventoryApp', ['firebase'])
+
+ var config = {
+    apiKey: "AIzaSyChr5X_3DHzmVYkNrdBSpqvDORHydaCApY",
+    authDomain: "inventory-app1.firebaseapp.com",
+    databaseURL: "https://inventory-app1.firebaseio.com",
+    storageBucket: "inventory-app1.appspot.com",
+    messagingSenderId: "627544312111"
+};
+
+firebase.initializeApp(config);
+
+
+angular.module('InventoryApp', ['firebase'])
+
+.controller('MyCtrl', function($scope, $firebaseObject){
+	const rootRef = firebase.database().ref().child('angular');
+	const ref = rootRef.child('object');
+	$scope.object = $firebaseObject(ref);
+})
+
+;
+}());
